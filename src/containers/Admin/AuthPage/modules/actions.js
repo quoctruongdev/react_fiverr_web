67c7507bff89ad2cdcae230ae263ccaf11302f8e
Quốc Trưởng <quoctruong_batboi@Quocs-MacBook-Pro.local>
2022-04-07 15:20:12 +0700

    update

--- src/containers/Admin/AuthPage/modules/actions.js
+++ src/containers/Admin/AuthPage/modules/actions.js
@@ -1,8 +1,7 @@
 import * as ActionType from "./constants";
 import { apiAdmin } from "./../../../../utils/apiutils";
 
-const TIME_EXP = 36000000000;
-// const TIME_EXP = 3600000;
+const TIME_EXP = 3600000;
 
 // "token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MTk4YWQ2NGFlZjM0NDAwMWNlZDA5NGEiLCJlbWFpbCI6ImFuaFR1eWV0QGdtYWlsLmNvbSIsInJvbGUiOiJBRE1JTiIsImlhdCI6MTY0MTYzNTA0Nn0.L4TaY1iZHjQE7UlbeMFbdtRY9BH3_32pOQcpYnFnz2U"
 
